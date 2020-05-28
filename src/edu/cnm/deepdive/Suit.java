0e01b316dc808d9a66a9a6b93e504f705ae3aeb7
levisanchez <levijames@gmail.com>
2020-05-28 11:06:30 -0600

    Added Javadoc

--- src/edu/cnm/deepdive/Suit.java
+++ src/edu/cnm/deepdive/Suit.java
@@ -1,9 +1,18 @@
 package edu.cnm.deepdive;
 
+/**
+ * Encapsulates the suits of standard playing cards. Each instance can return (using the {@link #symbol()}
+ * method) the Unicode symbol for that suit.
+ */
 public enum Suit {
 
   CLUBS, DIAMONDS, HEARTS, SPADES;
 
+  /**
+   * Returns the <em>unicode</em> symbol for the current instance.
+   *
+   * @return One of the four symbols: ♠, ♡, ♢, ♣.
+   */
   public char symbol(){
     switch (this) {
       case CLUBS:
